d73481e3bf13b84ba8d2cad0cad21b465b0491bc
Wilko Dekkers <wilko.dekkers@gmail.com>
2021-02-01 13:41:13 +0100

    Add: more test for lecture 1

--- Test/Lecture1Test.cpp
+++ Test/Lecture1Test.cpp
@@ -9,11 +9,27 @@ namespace Test
 {
 	TEST_CLASS(Lecture1Test)
 	{
+	private:
+		Lecture1 lecture1;
 	public:
-		
+		TEST_METHOD(TestMethodSum)
+		{
+			Assert::AreEqual(4, lecture1.sum(1, 3));
+			Assert::AreEqual(10, lecture1.sum(4, 6));
+			Assert::AreEqual(6, lecture1.sum(3, 3));
+			Assert::AreEqual(2, lecture1.sum(4, -2));
+			Assert::AreEqual(203, lecture1.sum(200, 3));
+		}
+
+		TEST_METHOD(TestMethodPower)
+		{
+			Assert::AreEqual(1, lecture1.power(5, 0));
+			Assert::AreEqual(5, lecture1.power(5, 1));
+			Assert::AreEqual(25, lecture1.power(5, 2));
+		}
+
 		TEST_METHOD(TestMethodIsPrime)
 		{
-			Lecture1 lecture1;
 			Assert::AreEqual(false, lecture1.isPrime(1));
 			Assert::AreEqual(true, lecture1.isPrime(2));
 			Assert::AreEqual(true, lecture1.isPrime(3));
